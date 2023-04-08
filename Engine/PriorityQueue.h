73f8c2997efc9bcd9614fad5b9a7ee97392e51bd
Syed Aman <syednashikaman@gmail.com>
2023-04-08 12:36:10 -0400

    Move out implementaitons outside of class scope

--- Engine/PriorityQueue.h
+++ Engine/PriorityQueue.h
@@ -9,69 +9,76 @@ class PriorityQueue
 {
     Container UnderlyingContainer;
 
-    void SiftUp(int CurrentIndex)
-    {
-        while (CurrentIndex > 0)
-        {
-            int ParentIndex = (CurrentIndex - 1) / 2;
-            T& Current = UnderlyingContainer[CurrentIndex];
-            T& Parent = UnderlyingContainer[ParentIndex];
+    void SiftUp(int CurrentIndex);
+    int GetHighestPriorityChildIndex(int CurrentIndex) const;
+    void SiftDown(int CurrentIndex);
 
-            if (bool bSatisfiesHeapProperty = Comparator()(Current, Parent))
-            {
-                Swap(Current, Parent);
-
-                CurrentIndex = ParentIndex;
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-
-    int GetHighestPriorityChildIndex(int CurrentIndex) const
-    {
-        int LeftChildIndex = CurrentIndex * 2 + 1;
-        int RightChildIndex = CurrentIndex * 2 + 2;
-        int ContainerSize = UnderlyingContainer.size();
-
-        if (LeftChildIndex >= ContainerSize) return -1; // No children, return -1.
-
-        if (RightChildIndex < ContainerSize && Comparator()(UnderlyingContainer[RightChildIndex], UnderlyingContainer[LeftChildIndex]))
-        {
-            return RightChildIndex;
-        }
-        else
-        {
-            return LeftChildIndex;
-        }
-    }
-
-    void SiftDown(int CurrentIndex)
-    {
-        int HighestPriorityChildIndex = GetHighestPriorityChildIndex(CurrentIndex);
-
-        bool bNoChildren = HighestPriorityChildIndex == -1;
-        if (bNoChildren) return;
-
-        T& Current = UnderlyingContainer[CurrentIndex];
-        T& HighestPriorityChild = UnderlyingContainer[HighestPriorityChildIndex];
-
-        bool bSatisfiesHeapProperty = Comparator()(HighestPriorityChild, Current);
-        if (bSatisfiesHeapProperty)
-        {
-            Swap(Current, HighestPriorityChild);
-            SiftDown(HighestPriorityChildIndex);
-        }
-    }
-    
 public:
     void push(T Value);
     bool empty();
     void pop();
     T top();
 };
+
+template <typename T, typename Container, typename Comparator>
+void PriorityQueue<T, Container, Comparator>::SiftUp(int CurrentIndex)
+{
+    while (CurrentIndex > 0)
+    {
+        int ParentIndex = (CurrentIndex - 1) / 2;
+        T& Current = UnderlyingContainer[CurrentIndex];
+        T& Parent = UnderlyingContainer[ParentIndex];
+
+        if (bool bSatisfiesHeapProperty = Comparator()(Current, Parent))
+        {
+            Swap(Current, Parent);
+
+            CurrentIndex = ParentIndex;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+template <typename T, typename Container, typename Comparator>
+int PriorityQueue<T, Container, Comparator>::GetHighestPriorityChildIndex(int CurrentIndex) const
+{
+    int LeftChildIndex = CurrentIndex * 2 + 1;
+    int RightChildIndex = CurrentIndex * 2 + 2;
+    int ContainerSize = UnderlyingContainer.size();
+
+    if (LeftChildIndex >= ContainerSize) return -1; // No children, return -1.
+
+    if (RightChildIndex < ContainerSize && Comparator()(UnderlyingContainer[RightChildIndex], UnderlyingContainer[LeftChildIndex]))
+    {
+        return RightChildIndex;
+    }
+    else
+    {
+        return LeftChildIndex;
+    }
+}
+
+template <typename T, typename Container, typename Comparator>
+void PriorityQueue<T, Container, Comparator>::SiftDown(int CurrentIndex)
+{
+    int HighestPriorityChildIndex = GetHighestPriorityChildIndex(CurrentIndex);
+
+    bool bNoChildren = HighestPriorityChildIndex == -1;
+    if (bNoChildren) return;
+
+    T& Current = UnderlyingContainer[CurrentIndex];
+    T& HighestPriorityChild = UnderlyingContainer[HighestPriorityChildIndex];
+
+    bool bSatisfiesHeapProperty = Comparator()(HighestPriorityChild, Current);
+    if (bSatisfiesHeapProperty)
+    {
+        Swap(Current, HighestPriorityChild);
+        SiftDown(HighestPriorityChildIndex);
+    }
+}
 
 template <typename T, typename Container, typename Comparator>
 void PriorityQueue<T, Container, Comparator>::push(T Value)
