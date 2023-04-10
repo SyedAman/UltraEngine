84b249284d8b3195306fa13f4612f81acf992db7
Syed Aman <syednashikaman@gmail.com>
2023-04-09 21:29:52 -0400

    Dynamically resize the array.

--- Engine/DynamicArray.h
+++ Engine/DynamicArray.h
@@ -1,29 +1,75 @@
 ﻿#pragma once
+#include <gtest/internal/gtest-death-test-internal.h>
 
 template <typename T>
 class DynamicArray
 {
-    int Size_ = 0;
+    size_t Size_ = 0;
     T* Data_;
+    size_t Capacity = 0;
+
+    void DoubleCapacity();
+    void DeallocateData() const;
+    void AllocateData(int NewSize);
 
 public:
     DynamicArray() : Data_(new T[Size_]) { }
     DynamicArray(int Size) : Size_(Size), Data_(new T[Size]) { }
 
+    size_t Size() const;
+    void PushBack(T Value);
+    void Clear();
     
-    int Size() const;
-    void push_back(T i);
     T& operator[](int i) { return Data_[i]; }
 };
 
 template <typename T>
-int DynamicArray<T>::Size() const
+size_t DynamicArray<T>::Size() const
 {
     return Size_;
 }
 
 template <typename T>
-void DynamicArray<T>::push_back(T i)
+void DynamicArray<T>::DoubleCapacity()
 {
-    Data_[Size_] = i;
+    size_t NewCapacity = Capacity > 0 ? Capacity * 2 : 1;
+    T* NewData = new T[NewCapacity];
+    for (size_t i = 0; i < Size_; i++)
+    {
+        NewData[i] = Data_[i];
+    }
+    DeallocateData();
+    Data_ = NewData;
+    Capacity = NewCapacity;
+}
+
+template <typename T>
+void DynamicArray<T>::PushBack(T Value)
+{
+    if (Size_ >= Capacity)
+    {
+            DoubleCapacity();
+    }
+    
+    Data_[Size_++] = Value;
+}
+
+template <typename T>
+void DynamicArray<T>::DeallocateData() const
+{
+    delete[] Data_;
+}
+
+template <typename T>
+void DynamicArray<T>::AllocateData(int NewSize)
+{
+    Data_ = new T[NewSize];
+    Size_ = NewSize;
+}
+
+template <typename T>
+void DynamicArray<T>::Clear()
+{
+    DeallocateData();
+    AllocateData(0);
 }
--- Engine/DynamicArray_Test.cpp
+++ Engine/DynamicArray_Test.cpp
@@ -16,14 +16,35 @@ TEST(DynamicArray, ShouldConstructWithSize)
     EXPECT_EQ(10, Array.Size());
 }
 
-TEST(DynamicArray, ShouldPushBack)
+TEST(DynamicArray, ShouldPushBackDynamically)
 {
     DynamicArray<int> Array;
-    Array.push_back(1);
+    Array.PushBack(1);
+    Array.PushBack(2);
+    Array.PushBack(3);
     EXPECT_EQ(1, Array[0]);
+    EXPECT_EQ(2, Array[1]);
+    EXPECT_EQ(3, Array[2]);
 }
 
-TEST(DynamicArray, ShouldNotHaveMemoryLeaks)
-{
-    // 
+TEST(DynamicArray, ShouldClear) {
+    DynamicArray<int> arr;
+
+    // Add some elements to the dynamic array
+    arr.PushBack(1);
+    arr.PushBack(2);
+    arr.PushBack(3);
+
+    // Clear the dynamic array
+    arr.Clear();
+
+    // Check if the Size is now 0
+    // EXPECT_EQ(arr.Size(), 0);
+
+    // Add another element to the dynamic array
+    // arr.PushBack(4);
+
+    // Check if the new element is added correctly
+    // EXPECT_EQ(arr.Size(), 1);
+    // EXPECT_EQ(arr[0], 4);
 }
