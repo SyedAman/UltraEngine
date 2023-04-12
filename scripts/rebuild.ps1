0665585335c60cbf457e95cb3b33a1085d63700e
Syed Aman <syednashikaman@gmail.com>
2023-04-12 19:23:34 -0400

    Rebuild using a powershell script without changing working directory and being able to run from anywhere.

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,4 +1,6 @@
-cd ..\build;
-Remove-Item -Path .\* -Recurse -Force;
-cmake ..;
-cmake --build .
+$scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$buildPath = Join-Path -Path $scriptPath -ChildPath "..\build"
+
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -Command { Remove-Item -Path .\* -Recurse -Force }" -WorkingDirectory $buildPath -NoNewWindow -Wait
+Start-Process -FilePath "cmake.exe" -ArgumentList ".." -WorkingDirectory $buildPath -NoNewWindow -Wait
+Start-Process -FilePath "cmake.exe" -ArgumentList "--build ." -WorkingDirectory $buildPath -NoNewWindow -Wait
