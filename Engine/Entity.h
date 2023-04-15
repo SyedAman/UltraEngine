95775f467bc19b5df9cbbfc94378d0cfcb77255a
Syed Aman <syednashikaman@gmail.com>
2023-04-15 18:39:31 -0400

    Start implementation of ECS framework. Create Entity class.

---
+++ Engine/Entity.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <compare>
+
+
+struct Entity
+{
+    int Id;
+    Entity(int Id) : Id(Id) {}
+    auto operator<=>(const Entity& entity) const = default;
+};
---
+++ Engine/Entity_Test.cpp
@@ -0,0 +1,66 @@
+#include "Entity.h"
+
+#include <gtest/gtest.h>
+
+TEST(Entity, ShouldCreateCreateWithPublicallyAccessibleID)
+{
+    const Entity entity1(0);
+    ASSERT_EQ(entity1.Id, 0);
+
+    const Entity entity2(1);
+    ASSERT_EQ(entity2.Id, 1);
+}
+
+TEST(Entity, ShouldCompareEqualityByID)
+{
+    const Entity entity1(5);
+    const Entity entity2(5);
+    const Entity entity3(7);
+
+    ASSERT_TRUE(entity1 == entity2);
+    ASSERT_FALSE(entity1 == entity3);
+
+    ASSERT_TRUE(entity1 != entity3);
+    ASSERT_FALSE(entity1 != entity2);
+}
+
+TEST(Entity, ShouldCompareUsingCustomOperators) {
+    Entity entity1(1);
+    Entity entity2(2);
+    Entity entity3(3);
+    ASSERT_TRUE(entity1 < entity2);
+    ASSERT_FALSE(entity1 > entity2);
+    ASSERT_TRUE(entity2 <= entity3);
+    ASSERT_TRUE(entity3 >= entity2);
+}
+
+TEST(Entity, ShouldCopyConstructWithSameID) {
+    Entity entity1(1);
+    Entity entity2(entity1);
+    ASSERT_EQ(entity1.Id, entity2.Id);
+}
+
+TEST(Entity, ShouldAssignWithSameID) {
+    Entity entity1(1);
+    Entity entity2(3);
+    entity2 = entity1;
+    ASSERT_EQ(entity1.Id, entity2.Id);
+}
+
+TEST(Entity, ShouldCompareEqualityForConstAndNonConstEntities) {
+    const Entity entity1(1);
+    Entity entity2(1);
+    const Entity entity3(2);
+
+    ASSERT_TRUE(entity1 == entity2);
+    ASSERT_FALSE(entity1 == entity3);
+
+    ASSERT_TRUE(entity1 != entity3);
+    ASSERT_FALSE(entity1 != entity2);
+}
+
+// TODO: These probably belong to a ComponentManager test suite. Move?
+TEST(Entity, ShouldAddComponentToEntity) {}
+TEST(Entity, ShouldRemoveComponentFromEntity) {}
+TEST(Entity, ShouldQueryEntityForComponent) {}
+TEST(Entity, ShouldGetComponentFromEntity) {}
