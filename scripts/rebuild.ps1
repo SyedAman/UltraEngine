ae974394b27d88827273bd4868bb378ef7b89796
Syed Aman <syednashikaman@gmail.com>
2023-04-17 14:07:57 -0400

    Update rebuild script to actually delete the entire folder contents

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,6 +1,8 @@
 $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $buildPath = Join-Path -Path $scriptPath -ChildPath "..\build"
 
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -Command { Remove-Item -Path .\* -Recurse -Force }" -WorkingDirectory $buildPath -NoNewWindow -Wait
+# Remove the build folder itself
+Get-ChildItem -Path $buildPath -Recurse | Remove-Item -Force -Recurse -ErrorAction Ignore
+
 Start-Process -FilePath "cmake.exe" -ArgumentList ".." -WorkingDirectory $buildPath -NoNewWindow -Wait
 Start-Process -FilePath "cmake.exe" -ArgumentList "--build ." -WorkingDirectory $buildPath -NoNewWindow -Wait
