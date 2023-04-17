9fda41c5a31f1ab4718aba6e4b051df35c605bd0
Syed Aman <syednashikaman@gmail.com>
2023-04-17 00:52:42 -0400

    Continue building out the HashMap.h

--- Engine/HashMap.h
+++ Engine/HashMap.h
@@ -1,8 +1,32 @@
 ﻿#pragma once
+#include "DynamicArray.h"
+#include "Pair.h"
 
-template <typename Key, typename Value>
+template <typename KeyType, typename ValueType>
 class HashMap
 {
-public:
+    DynamicArray<Pair<KeyType, ValueType>> Data = {};
     
+public:
+    void Insert(KeyType Key, ValueType Value);
+    ValueType operator[](KeyType Key);
 };
+
+template <typename KeyType, typename ValueType>
+void HashMap<KeyType, ValueType>::Insert(KeyType Key, ValueType Value)
+{
+    Data.PushBack(Pair<KeyType, ValueType>(Key, Value));
+}
+
+template <typename KeyType, typename ValueType>
+ValueType HashMap<KeyType, ValueType>::operator[](KeyType Key)
+{
+    // TODO: Currently O(n), make this O(1) with a hash table.
+    for (auto Pair : Data)
+    {
+        if (Pair.First == Key)
+        {
+            return Pair.Second;
+        }
+    }
+}
--- Engine/HashMap_Test.cpp
+++ Engine/HashMap_Test.cpp
@@ -1,14 +1,26 @@
 #include "HashMap.h"
+#include "String.h"
+#include "Pair.h"
 
 #include <gtest/gtest.h>
 
 TEST(HashMap, ShouldCreateWithTemplateForKeyAndValue)
 {
-    HashMap<int, int> hashMap;
+    HashMap<int, int> HashMap;
 }
 
 TEST(HashMap, ShouldInsertKeyValuePair)
 {
+    HashMap<int, int> HashMap1;
+    HashMap1.Insert(1, 1);
+    EXPECT_EQ(HashMap1[1], 1);
+
+    HashMap1.Insert(0, 7);
+    EXPECT_EQ(HashMap1[0], 7);
+
+    HashMap<String, Pair<int, String>> HashMap2;
+    HashMap2.Insert("Hello", Pair<int, String>(1, "World"));
+    EXPECT_EQ(HashMap2["Hello"].First, 1);
 }
 
 TEST(HashMap, ShouldUpdateValueForKey) {}
